da19124bbe0d54d2479642c04784630ddb1ff6c5
ashonbaiju <ashonbaiju123@gmail.com>
2026-05-09 16:12:35 +0530

    Fix React Router basename

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/alen">
+        <BrowserRouter basename="/alenportfolio">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
