d110838923479417793c608f2cec0c12f35798f9
ashonbaiju <ashonbaiju123@gmail.com>
2026-05-09 16:09:11 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/alen/",
+  base: "/alenportfolio/",
   server: {
     host: "::",
     port: 8080,
